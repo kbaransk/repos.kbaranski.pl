6cc06a881c46da4a53fd573347090ca6ed705d3d
Krzysztof Barański <kbaransk@gmail.com>
2015-09-07 01:00:04 +0200

    Excludes added to rsync

--- sync.sh
+++ sync.sh
@@ -3,6 +3,6 @@
 DIR=$(dirname $0)
 pushd ${DIR}
 
-/usr/bin/rsync --archive --verbose --compress --delete . ${REPOSITORY_REMOTE_HOST}:${REPOSITORY_REMOTE_DIR}
+/usr/bin/rsync --archive --verbose --compress --delete --exclude ".git**" . ${REPOSITORY_REMOTE_HOST}:${REPOSITORY_REMOTE_DIR}
 
 popd
