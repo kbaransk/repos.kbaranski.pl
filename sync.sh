dba7592d18549355ec608241c41eba836e70d99f
Krzysztof Barański <kbaransk@gmail.com>
2016-12-25 21:44:55 +0100

    Syncing only dists and pool directories

--- sync.sh
+++ sync.sh
@@ -3,6 +3,10 @@
 DIR=$(dirname $0)
 pushd ${DIR}
 
-/usr/bin/rsync --archive --verbose --compress --delete --exclude ".git**" . ${REPOSITORY_REMOTE_HOST}:${REPOSITORY_REMOTE_DIR}
+REPOSITORY_REMOTE_HOST=devil
+REPOSITORY_REMOTE_DIR=/home/kbaransk/domains/repos.kbaranski.pl/public_html/debian
+
+/usr/bin/rsync --archive --verbose --compress --delete --exclude ".git**" dists ${REPOSITORY_REMOTE_HOST}:${REPOSITORY_REMOTE_DIR}
+/usr/bin/rsync --archive --verbose --compress --delete --exclude ".git**" pool  ${REPOSITORY_REMOTE_HOST}:${REPOSITORY_REMOTE_DIR}
 
 popd
